3acabb591b8e918f51f17ec225efb36cb71a88ee
BabiSato <barbaranayumisato@gmail.com>
2025-04-16 21:08:45 -0300

    Update test_math_utils.py

--- test_math_utils.py
+++ test_math_utils.py
@@ -1,4 +1,4 @@
-from math_utils import add, subtract
+from math_utils import add, subtract, multiply
 
 def test_add():
   assert add(2, 3) == 5
@@ -10,7 +10,7 @@ def test_multiply():
   assert multiply(4, 3) == 12
 
 def test_multiply_invalid_type():
-try:
-  multiply("a", 3)
-except ValueError:
-  assert True
+  try:
+    multiply("a", 3)
+  except ValueError:
+    assert True
